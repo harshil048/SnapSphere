f1c4c18d257e52a51967a14004ad88aecccd0672
Harshil Bambhroliya <harshilbambhroliya048@gmail.com>
2023-11-18 20:16:35 +0530

    implement create post page

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -2,25 +2,43 @@ import {
   useQuery,
   useMutation,
   useQueryClient,
-  useInfiniteQuery
-} from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import {
+  createUserAccount,
+  signInAccount,
+  signOutAccount,
+  createPost,
+} from "../appwrite/api";
+import { INewPost, INewUser } from "@/types";
 
 export const useCreateUserAccount = () => {
   return useMutation({
-    mutationFn: (user: INewUser)=> createUserAccount(user)
+    mutationFn: (user: INewUser) => createUserAccount(user),
   });
 };
 
 export const useSignInAccount = () => {
   return useMutation({
-    mutationFn: (user: {email : string; password: string})=> signInAccount(user)
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
   });
 };
 
 export const useSignOutAccount = () => {
   return useMutation({
-    mutationFn: signOutAccount
+    mutationFn: signOutAccount,
+  });
+};
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["getRecentPosts"],
+      });
+    },
   });
 };
